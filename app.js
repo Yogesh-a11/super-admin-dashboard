85068f8ad9720c7007ae160b9bec1abf6f255430
yogesh kumar <yogeshgodara050@gmail.com>
2026-01-14 12:49:32 +0530

    route name updated

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
-import companyRouter from './Routes/companyRoute.js';
+import superAminRouter from './Routes/companyRoute.js';
 import { connectDB } from './config/db.js';
 
 const app = express();
 app.use(express.json());
 connectDB();
  
-app.use('/api/v1/companies', companyRouter)
+app.use('/api/v1/super-admin', superAminRouter)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
